0bfb7f225d431eaeec4226f7bd66e862b263f945
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2018-01-18 09:57:14 +0100

    Revert "Merge pull request #19 from kmorcinek/master"
    This reverts commit 89575d3c900a4bd46ffd121ba83af216582572dc, reversing
    changes made to 8bb0ba45d45090d6b0d7f91082c14d0ab27ece47.

--- Synergy.Contracts/Failures/DesignByContractViolationException.cs
+++ Synergy.Contracts/Failures/DesignByContractViolationException.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.Serialization;
 using JetBrains.Annotations;
 
 namespace Synergy.Contracts
@@ -8,6 +9,7 @@ namespace Synergy.Contracts
     /// The exception thrown when some contract check failed.
     /// When you see it it means that someone does not meet the contract.
     /// </summary>
+    [Serializable]
     [DebuggerStepThrough]
     public class DesignByContractViolationException : Exception
     {
@@ -23,6 +25,14 @@ namespace Synergy.Contracts
         /// </summary>
         /// <param name="message"></param>
         public DesignByContractViolationException([NotNull] string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Serialization required constructor.
+        /// </summary>
+        protected DesignByContractViolationException([NotNull] SerializationInfo info, StreamingContext context) :
+            base(info, context)
         {
         }
     }
