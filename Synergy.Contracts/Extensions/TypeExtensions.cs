826dc5306eaddf3ba072d4d57a410f5bfd5f874d
Cezary Piatek <cezarypiatek@live.com>
2018-08-21 22:57:25 +0200

    Convert Sample and Test to vs2017 project format. Remove duplicated ExcludeFromCodeCoverageAttribute. Disable FxCope analysis. Convert Synergy.Contracts to multitarget project to support netstandard2.0 and net462

--- Synergy.Contracts/Extensions/TypeExtensions.cs
+++ Synergy.Contracts/Extensions/TypeExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Reflection;
+using JetBrains.Annotations;
 
 namespace Synergy.Contracts.Extensions
 {
     internal static class TypeExtensions
     {
-        public static bool IsInstanceOfType(this Type type, object obj)
+        public static bool IsInstanceOfType(this Type type, [CanBeNull] object obj)
         {
             return obj != null && type.GetTypeInfo().IsAssignableFrom(obj.GetType().GetTypeInfo());
         }
